eca9f02a5bd7cd28115775e659d07fdc0fd0a890
Julio Macedo <juliocesar1076@gmail.com>
2022-07-21 23:47:03 -0300

    fix: toggle input visibility

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -2,21 +2,21 @@ import styled, { css } from 'styled-components';
 interface formProps {
   isVisible: boolean;
 }
-export const Container = styled.form<formProps>`
-  display: none;
+export const Container = styled.div<formProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
   opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
-  transition: opacity 0.3s;
-  margin-bottom: 15px;
+  transition: visibility 0s, opacity 0.5s linear;
+  margin-bottom: 10px;
 `;
 
 export const Input = styled.input`
+  width: 100%;
   border-radius: 8px;
   padding: 5px;
   outline: none;
-  margin-right: 10px;
   ${({ theme }) => css`
     border: 1px solid ${theme.colors.primary};
   `}

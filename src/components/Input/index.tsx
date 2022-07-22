ffaad3a99d70ca63240194946b0810cdb8b35f31
Julio Macedo <juliocesar1076@gmail.com>
2022-07-21 23:48:14 -0300

    fix: add events

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,48 @@
+import { SetStateAction } from 'react';
 import * as S from './style';
+
 interface inputProps {
   title: string;
   setTitle: (arg0: string) => void;
   addTask: (arg0: string) => void;
   isVisible: boolean;
+  setIsVisible: React.Dispatch<SetStateAction<boolean>>;
 }
-export const Input = ({ setTitle, addTask, title, isVisible }: inputProps) => {
+
+export const Input = ({
+  setTitle,
+  setIsVisible,
+  addTask,
+  title,
+  isVisible,
+}: inputProps) => {
+  function handleAddTask() {
+    addTask(title);
+    setTitle('');
+    setIsVisible(false);
+  }
+
   return (
     <S.Container isVisible={isVisible}>
-      <S.Input onChange={(e) => setTitle(e.target.value)} />
-      <S.Button type="button" onClick={() => addTask(title)}>
-        Add task
-      </S.Button>
+      <S.Input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        onBlur={() => {
+          if (title.trim().length) {
+            handleAddTask();
+          } else {
+            setIsVisible(false);
+          }
+        }}
+        onKeyDown={(e) => {
+          if (e.code === 'Enter' && title.trim().length) {
+            handleAddTask();
+          } else if (e.code === 'Escape') {
+            setIsVisible(false);
+            setTitle('');
+          }
+        }}
+      />
     </S.Container>
   );
 };
